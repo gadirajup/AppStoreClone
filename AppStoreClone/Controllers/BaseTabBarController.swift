024ba185352a735b981c8487f787df306bfb73b4
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2019-04-08 20:35:45 -0700

    - Setup Base Tab Controller - Setup Search Collection View Controller

---
+++ AppStoreClone/Controllers/BaseTabBarController.swift
@@ -0,0 +1,41 @@
+//
+//  BaseTabBarController.swift
+//  AppStoreClone
+//
+//  Created by Prudhvi Gadiraju on 4/8/19.
+//  Copyright © 2019 Prudhvi Gadiraju. All rights reserved.
+//
+
+import UIKit
+
+class BaseTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupTabs()
+    }
+    
+    fileprivate func setupTabs() {
+        viewControllers = [
+            createNavController(viewController: ViewController(), title: "Today", image: #imageLiteral(resourceName: "today_icon")),
+            createNavController(viewController: ViewController(), title: "App", image: #imageLiteral(resourceName: "apps")),
+            createNavController(viewController: SearchController(), title: "Search", image: #imageLiteral(resourceName: "search"))
+        ]
+    }
+    
+    fileprivate func createNavController(viewController: UIViewController, title: String, image: UIImage) -> UINavigationController {
+        // Set Title
+        viewController.navigationItem.title = title
+        viewController.view.backgroundColor = .white
+        
+        // Create Nav Controller
+        let navigationController = UINavigationController(rootViewController: viewController)
+        navigationController.navigationBar.prefersLargeTitles = true
+        navigationController.tabBarItem.title = title
+        navigationController.tabBarItem.image = image
+        
+        // Return
+        return navigationController
+    }
+}
---
+++ AppStoreClone/Controllers/SearchController.swift
@@ -0,0 +1,26 @@
+//
+//  SearchController.swift
+//  AppStoreClone
+//
+//  Created by Prudhvi Gadiraju on 4/8/19.
+//  Copyright © 2019 Prudhvi Gadiraju. All rights reserved.
+//
+
+import UIKit
+
+class SearchController: UICollectionViewController {
+    
+    init() {
+        super.init(collectionViewLayout: UICollectionViewFlowLayout())
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.backgroundColor = .white
+    }
+}
